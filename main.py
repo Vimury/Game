143542c1673bcc373f8c5de7cb0ac9378072300c
Game <vova.dzhioev.07@mail.ru>
2022-12-19 22:24:15 +0300

    Commit massage

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from Classes import Map, Cell
+import pygame
+import os
+import sys
 
 '''Прописал id государств'''
 red = 1
@@ -29,17 +32,61 @@ villager = 10
 knight = 12
 big_knight = 13
 
-
-
 '''Прописал id типа местности'''
 water = 0
 ground = 1
 
 
-def main():
-    x, y = 30, 30
-    m = Map(x, y)
+def render():  # Загрузка стартовой земли
+    for x in range(x_size):
+        for y in range(y_size):
+            for step in range(0, 42 * x_size, 63):
+                # Почему-то только воду делает и не могу понять почему
+                if (a := m.map[x][y].type) == ground:
+                    cell = load_image('ground.png', (237, 28, 36))
+                elif a == water:
+                    cell = load_image('water.png', (237, 28, 36))
+                if y & 1 == 0:  # Быстрая проверка на чётность
+                    screen.blit(cell, (step, y * 15))
+                else:
+                    screen.blit(cell, (step + 31, y * 15))
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
 
 
 if __name__ == '__main__':
-    main()
+    x_size, y_size = 20, 20
+    m = Map(x_size, y_size)
+    pygame.init()
+    pygame.display.set_caption('Countries of century knights')
+    size = width, height = 1000, 750
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+    fps = 30
+
+    screen.fill((0, 0, 0))
+    clock = pygame.time.Clock()
+    running = True
+    render()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        clock.tick(fps)
+        pygame.display.flip()
+    pygame.quit()
